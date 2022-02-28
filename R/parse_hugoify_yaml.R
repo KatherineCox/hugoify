60108545a9ea3ec358aa0f5824210752d7464e1a
Katherine Cox <katherinecox@jhu.edu>
2022-02-27 23:24:53 -0500

    work on parse_hugoify_yaml

--- R/parse_hugoify_yaml.R
+++ R/parse_hugoify_yaml.R
@@ -1,5 +1,7 @@
 # TODO: parse_hugoify_yaml defaults to hugoify.yml or hugoify.yaml
-# TODO: what should parse_hugoify_yaml
+# TODO: what exactly should parse_hugoify_yaml return?
+#         - a collection of calls, but how should this be organized?
+# TODO: should parse_hugoify_yaml accept an output_dir arg?
 
 parse_hugoify_yaml <- function( yaml_file ) {
 
@@ -9,6 +11,12 @@ parse_hugoify_yaml <- function( yaml_file ) {
   # should be something like:
   # y <- yaml::yaml.load(yaml, handlers = list(img = make_test_image(x)))
 
+  # get output directory
+  output_dir <- y[["output_dir"]]
+  if ( is.null(output_dir) ) {
+    output_dir <- "."
+  }
+
   # content can be under the "content" or "home" key
   content <- "content" %in% names(y)
   home <- "home" %in% names(y)
@@ -17,13 +25,29 @@ parse_hugoify_yaml <- function( yaml_file ) {
     stop("found both 'content' and 'home' keys in hugoify yaml config.\n",
          "hugoify must have either 'content' or 'home' but not both.")
   } else if (content) {
-    #return( construct_page_calls(y[["content"]]) )
+    #calls <- construct_page_calls(y[["content"]]
   } else if (home) {
-    #return( construct_page_calls(y[["home"]]) )
+    #calls <- construct_page_calls(y[["home"]])
   } else {
     stop("no site content found.\n",
          "hugoify yaml config must have a 'content' or 'home' key.")
   }
+
+  # if ( is.null(y[["config"]]) ) {
+  #   # error
+  # } else {
+  #   #calls[[length(calls) + 1]] <- file.copy(y[["config"]],
+  #                                               #destination,
+  #                                               #recursive=TRUE)
+  # }
+  #
+  # if ( is.null(y[["theme"]]) ) {
+  #   # error
+  # } else {
+  #   #calls[[length(calls) + 1]] <- file.copy(y[["theme"]],
+  #   #destination,
+  #   #recursive=TRUE)
+  # }
 
 }
 
--- tests/testthat/test-parse_hugoify_yaml.R
+++ tests/testthat/test-parse_hugoify_yaml.R
@@ -1,23 +1,36 @@
-test_that("parse_hugoify_yaml checks for site content", {
+test_that("parse_hugoify_yaml checks for required keys", {
 
   yaml_file <- withr::local_file("hugoify.yml")
 
   # file with both "content" and "home" keys returns an error
-  yaml <- "---\ncontent:\nhome:\n---"
+  yaml <- paste("---", "content:", "home:", "config:", "theme:", "---", sep="\n")
   writeLines(yaml, yaml_file)
   expect_error(parse_hugoify_yaml(yaml_file), regexp="found both 'content' and 'home' keys")
 
   # file with neither "content" nor "home" keys returns an error
-  yaml <- "---\n---"
+  yaml <- paste("---", "config:", "theme:", "---", sep="\n")
   writeLines(yaml, yaml_file)
   expect_error(parse_hugoify_yaml(yaml_file), regexp="no site content found")
 
-  # file with either a "content" or "home" key does not error
-  yaml <- "---\ncontent:\n---"
+  # file with no config key returns an error
+  yaml <- paste("---", "content:", "theme:", "---", sep="\n")
+  writeLines(yaml, yaml_file)
+  expect_error(parse_hugoify_yaml(yaml_file), regexp="fix me")
+
+  # file with no theme key returns an error
+  yaml <- paste("---", "content:", "config:", "---", sep="\n")
+  writeLines(yaml, yaml_file)
+  expect_error(parse_hugoify_yaml(yaml_file), regexp="fix me")
+
+  # file with all required keys does not error
+  # - "content" or "home"
+  # - "config"
+  # - "theme"
+  yaml <- paste("---", "content:", "config:", "theme:", "---", sep="\n")
   writeLines(yaml, yaml_file)
   expect_error(parse_hugoify_yaml(yaml_file), regexp=NA)
 
-  yaml <- "---\nhome:\n---"
+  yaml <- paste("---", "home:", "config:", "theme:", "---", sep="\n")
   writeLines(yaml, yaml_file)
   expect_error(parse_hugoify_yaml(yaml_file), regexp=NA)
 
