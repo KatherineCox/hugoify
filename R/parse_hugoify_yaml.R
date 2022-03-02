86144ddf8c30aaa33f60b5ec3a5155242afb9efb
Katherine Cox <katherinecox@jhu.edu>
2022-03-01 23:02:54 -0500

    errors and warnings for parse_hugoify_yaml

--- R/parse_hugoify_yaml.R
+++ R/parse_hugoify_yaml.R
@@ -2,8 +2,9 @@
 # TODO: what exactly should parse_hugoify_yaml return?
 #         - a collection of calls, but how should this be organized?
 # TODO: should parse_hugoify_yaml accept an output_dir arg?
+# TODO: should overwrite=FALSE (in e.g. file.copy)?
 
-parse_hugoify_yaml <- function( yaml_file ) {
+parse_hugoify_yaml <- function( yaml_file, output_dir=NULL, quiet=FALSE ) {
 
   y <- yaml::yaml.load_file(yaml_file)
 
@@ -11,9 +12,14 @@ parse_hugoify_yaml <- function( yaml_file ) {
   # should be something like:
   # y <- yaml::yaml.load(yaml, handlers = list(img = make_test_image(x)))
 
+
   # get output directory
-  output_dir <- y[["output_dir"]]
-  if ( is.null(output_dir) ) {
+  # arg takes priority. if it's NULL, then check the yaml
+  if (is.null(output_dir)) {
+    output_dir <- y[["output_dir"]]
+  }
+  # if there was nothing in the yaml, use the working directory
+  if (is.null(output_dir)) {
     output_dir <- "."
   }
 
@@ -22,33 +28,42 @@ parse_hugoify_yaml <- function( yaml_file ) {
   home <- "home" %in% names(y)
 
   if (content & home) {
-    stop("found both 'content' and 'home' keys in hugoify yaml config.\n",
+    stop("Found both 'content' and 'home' keys in hugoify yaml config.\n",
          "hugoify must have either 'content' or 'home' but not both.")
   } else if (content) {
-    #calls <- construct_page_calls(y[["content"]]
+    #calls <- construct_page_calls( y[["content"]],
+    #                               output_dir=file.path(output_dir, "content"))
   } else if (home) {
-    #calls <- construct_page_calls(y[["home"]])
+    #calls <- construct_page_calls( y[["home"]],
+    #                               output_dir=file.path(output_dir, "content"))
   } else {
-    stop("no site content found.\n",
+    stop("No site content found.\n",
          "hugoify yaml config must have a 'content' or 'home' key.")
   }
 
-  # if ( is.null(y[["config"]]) ) {
-  #   # error
-  # } else {
+  if ( is.null(y[["config"]]) ) {
+    # warning rather than error, in case someone just wants to build the content
+    if (quiet==FALSE) {
+      warning("No hugo config file specified.")
+    }
+  } else {
   #   #calls[[length(calls) + 1]] <- file.copy(y[["config"]],
   #                                               #destination,
   #                                               #recursive=TRUE)
-  # }
-  #
-  # if ( is.null(y[["theme"]]) ) {
-  #   # error
-  # } else {
+  }
+
+  if ( is.null(y[["theme"]]) ) {
+    # warning rather than error, in case someone just wants to build the content
+    if (quiet==FALSE) {
+      warning("No hugo theme directory specified.")
+    }
+  } else {
   #   #calls[[length(calls) + 1]] <- file.copy(y[["theme"]],
   #   #destination,
   #   #recursive=TRUE)
-  # }
+  }
 
+  calls
 }
 
 construct_page_calls <- function() {
--- tests/testthat/test-parse_hugoify_yaml.R
+++ tests/testthat/test-parse_hugoify_yaml.R
@@ -1,37 +1,47 @@
-test_that("parse_hugoify_yaml checks for required keys", {
+# TODO: test parse_hugoify_yaml puts things in the right directory
+
+
+test_that("parse_hugoify_yaml checks for expected keys", {
 
   yaml_file <- withr::local_file("hugoify.yml")
 
   # file with both "content" and "home" keys returns an error
-  yaml <- paste("---", "content:", "home:", "config:", "theme:", "---", sep="\n")
+  yaml <- paste("---", "content:", "home:", "config: c", "theme: t", "---", sep="\n")
   writeLines(yaml, yaml_file)
-  expect_error(parse_hugoify_yaml(yaml_file), regexp="found both 'content' and 'home' keys")
+  expect_error(parse_hugoify_yaml(yaml_file), regexp="Found both 'content' and 'home' keys")
 
   # file with neither "content" nor "home" keys returns an error
-  yaml <- paste("---", "config:", "theme:", "---", sep="\n")
+  yaml <- paste("---", "config: c", "theme: t", "---", sep="\n")
   writeLines(yaml, yaml_file)
-  expect_error(parse_hugoify_yaml(yaml_file), regexp="no site content found")
+  expect_error(parse_hugoify_yaml(yaml_file), regexp="No site content found")
 
-  # file with no config key returns an error
-  yaml <- paste("---", "content:", "theme:", "---", sep="\n")
+  # file with no config key returns a warning
+  # and this warning is suppressed by 'quiet=TRUE'
+  yaml <- paste("---", "content:", "theme: t", "---", sep="\n")
   writeLines(yaml, yaml_file)
-  expect_error(parse_hugoify_yaml(yaml_file), regexp="fix me")
+  expect_warning(parse_hugoify_yaml(yaml_file), regexp="No hugo config file specified.")
+  expect_warning(parse_hugoify_yaml(yaml_file, quiet=TRUE), regexp=NA)
 
-  # file with no theme key returns an error
-  yaml <- paste("---", "content:", "config:", "---", sep="\n")
+
+  # file with no theme key returns a warning
+  yaml <- paste("---", "content:", "config: c", "---", sep="\n")
   writeLines(yaml, yaml_file)
-  expect_error(parse_hugoify_yaml(yaml_file), regexp="fix me")
+  expect_warning(parse_hugoify_yaml(yaml_file), regexp="No hugo theme directory specified.")
+  expect_warning(parse_hugoify_yaml(yaml_file, quiet=TRUE), regexp=NA)
 
-  # file with all required keys does not error
+  # no theme warning is suppressed by 'quiet=TRUE' arg
+
+  # file with all expected keys does not generate error or warning
   # - "content" or "home"
   # - "config"
   # - "theme"
-  yaml <- paste("---", "content:", "config:", "theme:", "---", sep="\n")
+  yaml <- paste("---", "content:", "config: c", "theme: t", "---", sep="\n")
   writeLines(yaml, yaml_file)
   expect_error(parse_hugoify_yaml(yaml_file), regexp=NA)
 
-  yaml <- paste("---", "home:", "config:", "theme:", "---", sep="\n")
+  yaml <- paste("---", "home:", "config: c", "theme: t", "---", sep="\n")
   writeLines(yaml, yaml_file)
   expect_error(parse_hugoify_yaml(yaml_file), regexp=NA)
 
 })
+
