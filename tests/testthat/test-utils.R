62f2c43d1688722a1bf303dfe3de21f92a51c974
Katherine Cox <katherinecox@jhu.edu>
2022-03-05 15:34:11 -0500

    add another write_md test

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -13,12 +13,26 @@ test_that("write_md outputs the expected markdown file", {
 })
 
 test_that("write_md handles empty yaml", {
-  yaml <- list()
+
+  # no yaml provided
   content <- "This test file was written by write_md()."
   filename <- "write_md_no_yaml.md"
   output_dir <- withr::local_tempdir()
 
+  write_md(content=content, filename=filename, output_dir=output_dir)
+  expect_snapshot_file(file.path(output_dir, filename))
+
+  # provided yaml is empty list
+  yaml <- list()
+  content <- "This test file was written by write_md()."
+  filename <- "write_md_empty_yaml.md"
+  output_dir <- withr::local_tempdir()
+
   write_md(yaml=yaml, content=content, filename=filename, output_dir=output_dir)
   expect_snapshot_file(file.path(output_dir, filename))
+})
+
+test_that("write_md handles empty content", {
+
 })
 
